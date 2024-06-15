770f18ac93a25c8c0d72ac6e879cf2662041db49
Rudr2004 <161303778+Rudr2004@users.noreply.github.com>
2024-06-15 18:40:15 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -83,7 +83,7 @@ async function displayAlbums() {
     for (let index = 0; index < array.length; index++) {
       const e = array[index];
   
-    if(e.href.includes("songs/")){
+    if(e.href.includes("songs/") && !e.href.includes(".htaccess")){
     let folder = e.href.split("songs/").slice(-2)[1]
     //Get the metadata of the folder
     let a = await fetch(`songs/${folder}/info.json`);
